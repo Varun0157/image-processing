1225e8658f4e8dbb9efd56af6f52ccb46c5681cc
Varun0157 <varun.edachali@gmail.com>
2025-01-13 17:10:43 +0530

    feat: thresholding using Otsu's method
    massively better results

--- Cloverleaf Bridge/src/preprocessing.py
+++ Cloverleaf Bridge/src/preprocessing.py
@@ -4,18 +4,27 @@ import numpy as np
 from src.utils import show_image
 
 
+# TODO: consider creating an iterable of ops and applying them. Cleaner.
 def preprocess_image(img: np.ndarray) -> np.ndarray:
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    show_image(gray, "gray image", cmap="gray")
+    L = np.iinfo(img.dtype).max + 1
 
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)  # kernel size, sigma
-    show_image(blurred, "blurred image", cmap="gray")
+    out = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    show_image(out, "gray", cmap="gray")
 
-    high_contrast = cv2.equalizeHist(blurred)
-    show_image(high_contrast, "high contrast image", cmap="gray")
+    out = cv2.GaussianBlur(out, (5, 5), 0)  # kernel size, sigma
+    show_image(out, "blurred", cmap="gray")
 
-    edge_detected = cv2.Canny(high_contrast, 30, 150)  # threshold1, threshold2
-    show_image(edge_detected, "edge detected image", cmap="gray")
+    _, out = cv2.threshold(out, 0, L, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    show_image(out, "thresholded", cmap="gray")
 
-    out = edge_detected
+    out = cv2.equalizeHist(out)
+    show_image(out, "histogram equalised", cmap="gray")
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    out = cv2.morphologyEx(out, cv2.MORPH_CLOSE, kernel)
+    show_image(out, "morphed", cmap="gray")
+
+    out = cv2.Canny(out, 30, 150)  # threshold1, threshold2
+    show_image(out, "edge detected", cmap="gray")
+
     return out
