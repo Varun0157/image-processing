e7cb6df32e679b795f0c9e2790cd6b008a3da25e
Varun0157 <varun.edachali@gmail.com>
2025-01-15 16:54:32 +0530

    feat: better heuristic for cloverleaf determination
    removing hardcoded radii to ensure a more general formulation

--- Cloverleaf Bridge/src/detection.py
+++ Cloverleaf Bridge/src/detection.py
@@ -1,13 +1,15 @@
 import logging
+import math
 
 import cv2
+from cv2.typing import MatLike
 import numpy as np
 
 from src.utils import show_image
 
 
 # TODO: consider creating an iterable of ops and applying them. Cleaner?
-def preprocess_image(img: np.ndarray) -> np.ndarray:
+def detect_cloverleaves(img: np.ndarray) -> np.ndarray:
     logging.info("pre-processing image for further analysis ... ")
 
     L = np.iinfo(img.dtype).max + 1
@@ -32,94 +34,117 @@ def preprocess_image(img: np.ndarray) -> np.ndarray:
     out = cv2.dilate(out, kernel, iterations=3)
     show_image(out, "dilated", False, cmap="gray")
 
+    def is_cloverleaf(contour: MatLike) -> bool:
+        perim = cv2.arcLength(contour, True)
+        rad = perim / (2 + (3 / 2) * math.pi)
+        (x, y), enc_rad = cv2.minEnclosingCircle(contour)
+        # cv2.circle(
+        #     out,
+        #     (int(x), int(y)),
+        #     radius=int(enc_rad),
+        #     color=(255, 255, 255),
+        #     thickness=2,
+        # )
+
+        area = cv2.contourArea(contour)
+
+        tolerance = 0.17
+
+        circle_area = (3 / 4) * math.pi * rad**2
+        square_area = rad**2
+        total_area = circle_area + square_area
+
+        # TODO: remove later
+        is_cl_test = 200 <= enc_rad <= 250
+        logging.info(
+            f"{is_cl_test}\t: {(area - total_area) / area}, {area}, {total_area}"
+        )
+
+        return abs((area - total_area) / total_area) < tolerance
+
     contours, _ = cv2.findContours(out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-        (x, y), rad = cv2.minEnclosingCircle(contour)
-        # NOTE: manually logged contour shapes to debug and find the correct radii
-        # TODO: clean
-        if rad < 200 or rad > 250:
-            cv2.drawContours(out, [contour], -1, (0, 0, 0), -1)
-            continue
-        logging.info(f"drawing countour at {x}, {y} with radius {rad}")
-        cv2.drawContours(out, [contour], 0, (255, 255, 255), -1)
+        colour = (0, 0, 0) if not is_cloverleaf(contour) else (255, 255, 255)
+        cv2.drawContours(out, [contour], 0, colour, -1)
 
     show_image(out, "contours filled", False, cmap="gray")
 
     out = cv2.morphologyEx(out, cv2.MORPH_CLOSE, kernel, iterations=5)
     show_image(out, "morphed - close", False, cmap="gray")
 
+    out = cv2.morphologyEx(out, cv2.MORPH_OPEN, kernel, iterations=5)
+    show_image(out, "morphed - open", False, cmap="gray")
+
     return out
 
 
-# def _find_circles(img: np.ndarray) -> np.ndarray:
-#     circles = cv2.HoughCircles(
-#         img,
-#         cv2.HOUGH_GRADIENT,
-#         dp=1,
-#         minDist=100,
-#         param1=50,
-#         param2=50,
-#         minRadius=150,
-#         maxRadius=300,
-#     )
-#
-#     return circles
+def _find_circles(img: np.ndarray) -> np.ndarray | None:
+    circles = cv2.HoughCircles(
+        img,
+        cv2.HOUGH_GRADIENT,
+        dp=2,
+        minDist=100,
+        param1=50,
+        param2=30,
+        minRadius=150,
+        maxRadius=200,
+    )
+
+    return circles
 
 
-# def _mark_circles(img: np.ndarray, circles: None | np.ndarray) -> np.ndarray:
-#     if circles is None:
-#         logging.info("no circles found")
-#         return img
-#
-#     annotated = img.copy()
-#
-#     OUTER_COLOUR, CENTER_COLOUR = (0, 255, 0), (255, 0, 0)
-#     circles = np.round(circles).astype("int")
-#     for i in circles[0, :]:
-#         center, rad = (i[0], i[1]), i[2]
-#         cv2.circle(annotated, center, rad, OUTER_COLOUR, 2)
-#         cv2.circle(annotated, center, 2, CENTER_COLOUR, 3)
-#
-#         cv2.putText(
-#             annotated,
-#             f"r={rad}",
-#             (center[0] + rad // 4, center[1] + rad // 4),
-#             cv2.FONT_HERSHEY_SIMPLEX,
-#             1,
-#             OUTER_COLOUR,
-#             2,
-#         )
-#
-#         center_text = f"({int(center[0])},{int(center[1])})"
-#         cv2.putText(
-#             annotated,
-#             center_text,
-#             (center[0] - rad // 4, center[1] - rad // 4),
-#             cv2.FONT_HERSHEY_SIMPLEX,
-#             1,
-#             CENTER_COLOUR,
-#             2,
-#         )
-#
-#         logging.info(f"\tcircle at {center_text} with radius {rad}")
-#
-#     return annotated
+def _mark_circles(img: np.ndarray, circles: np.ndarray | None) -> np.ndarray:
+    if circles is None:
+        logging.info("no circles found")
+        return img
+
+    annotated = img.copy()
+
+    OUTER_COLOUR, CENTER_COLOUR = (0, 255, 0), (255, 0, 0)
+    circles = np.round(circles).astype("int")
+    for i in circles[0, :]:
+        center, rad = (i[0], i[1]), i[2]
+        cv2.circle(annotated, center, rad, OUTER_COLOUR, 2)
+        cv2.circle(annotated, center, 2, CENTER_COLOUR, 3)
+
+        cv2.putText(
+            annotated,
+            f"r={rad}",
+            (center[0] + rad // 4, center[1] + rad // 4),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            OUTER_COLOUR,
+            2,
+        )
+
+        center_text = f"({int(center[0])},{int(center[1])})"
+        cv2.putText(
+            annotated,
+            center_text,
+            (center[0] - rad // 4, center[1] - rad // 4),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            CENTER_COLOUR,
+            2,
+        )
+
+        logging.info(f"\tcircle at {center_text} with radius {rad}")
+
+    return annotated
 
 
 # TODO: create a mark_radii option and use that to distinguish between 2.2.1 and 2.2.2
 def mark_circles(img: np.ndarray) -> np.ndarray:
-    # NOTE: issue faced - too many circles -> asked Claude for recommendations
-    # show some of the other params messed with as well. Took a while to reach this result.
-    # also mention that asking for how to find circles is what led you to HoughCircles in the first place.
-    # show the code in the docs as a reference.
-
-    # logging.info("finding circles in the raw image ... ")
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # circles = _find_circles(gray)
-    # annotated = _mark_circles(img, circles)
-    # show_image(annotated, "raw circles", save=True, cmap="gray")
+    logging.info("finding circles in the raw image ... ")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    circles = _find_circles(gray)
+    annotated = _mark_circles(img, circles)
+    show_image(annotated, "raw circles", save=True, cmap="gray")
 
     logging.info("processing images and finding circles in the image ... ")
-    processed = preprocess_image(img)
+    processed = detect_cloverleaves(img)
 
-    return processed
+    circles = _find_circles(processed)
+    out = _mark_circles(img, circles)
+
+    return out
