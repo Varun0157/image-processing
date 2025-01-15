f596267f79c3f94b0e54014fb804dc56c6177233
Varun0157 <varun.edachali@gmail.com>
2025-01-15 11:46:09 +0530

    feat: primary processing of language doc

--- Line Segmentation in Historical Document/q2.py
+++ Line Segmentation in Historical Document/q2.py
@@ -1,11 +1,15 @@
 import logging
 
 from src.utils import load_historical_doc, show_image
+from src.detection import preprocess_image
 
 
 def main() -> None:
     img = load_historical_doc()
     show_image(img, "initial image", save=False)
+
+    annotated = preprocess_image(img)
+    show_image(annotated, "processed", False)
 
 
 if __name__ == "__main__":
