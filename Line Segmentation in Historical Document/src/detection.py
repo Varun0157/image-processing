e901c45a31f792605a04ebed19a8ce1838352275
Varun0157 <varun.edachali@gmail.com>
2025-01-22 15:53:24 +0530

    feat: bottom half processing for non-seal bb's

--- Line Segmentation in Historical Document/src/detection.py
+++ Line Segmentation in Historical Document/src/detection.py
@@ -13,23 +13,27 @@ def polygon_bound(img: np.ndarray) -> np.ndarray:
     logging.info("pre-processing image for further analysis ... ")
 
     L = np.iinfo(img.dtype).max + 1
+    y_sep = 85
 
     out = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     show_image(out, "gray", False, cmap="gray")
 
-    out = cv2.GaussianBlur(out, (5, 5), 0)  # kernel size, sigma
+    out[y_sep:, :] = cv2.GaussianBlur(out[y_sep:, :], (5, 5), 0)  # kernel size, sigma
     show_image(out, "blurred", False, cmap="gray")
 
-    _, out = cv2.threshold(out, 0, L, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    _, out[y_sep:, :] = cv2.threshold(
+        out[y_sep:, :], 0, L, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
     show_image(out, "thresholded", False, cmap="gray")
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    out = cv2.morphologyEx(out, cv2.MORPH_OPEN, kernel)
+    out[y_sep:, :] = cv2.morphologyEx(out[y_sep:, :], cv2.MORPH_OPEN, kernel)
     show_image(out, "morphed", False, cmap="gray")
 
     contours, hierarchy = cv2.findContours(out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
+        logging.info(f"bounding box: {x, y, w, h}")
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     show_image(img, "bounding boxes", False)
 
