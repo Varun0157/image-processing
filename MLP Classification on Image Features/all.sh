7bcc01871ad300f2c8046e807d7e14113613a3b7
Varun0157 <varun.edachali@gmail.com>
2025-01-23 20:33:17 +0530

    res: 3e-6 lr

--- MLP Classification on Image Features/all.sh
+++ MLP Classification on Image Features/all.sh
@@ -1,11 +1,11 @@
 echo "RAW"
-python -m src.q3 --transform raw --lr 5e-4
+python -m src.q3 --transform raw --lr 3e-6
 
 echo && echo "blurred and equalised"
-python -m src.q3 --transform blur_equal --lr 5e-4
+python -m src.q3 --transform blur_equal --lr 3e-6
 
 echo && echo "edge detected"
-python -m src.q3 --transform edge_detect --lr 5e-4
+python -m src.q3 --transform edge_detect --lr 3e-6
 
-echo && echo "hog features"
-python -m src.q3 --transform hog_feat --lr 5e-4
+# echo && echo "hog features"
+# python -m src.q3 --transform hog_feat --lr 5e-4
