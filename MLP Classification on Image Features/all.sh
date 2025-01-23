afe4e9cc63612c98e6c71d250a9c815e7375ffa8
Varun0157 <varun.edachali@gmail.com>
2025-01-23 18:44:20 +0530

    chore: local wandb runs (temp)

--- MLP Classification on Image Features/all.sh
+++ MLP Classification on Image Features/all.sh
@@ -1,11 +1,11 @@
 echo "RAW"
-python -m src.q3 --transform raw
+python -m src.q3 --transform raw --lr 5e-4
 
 echo && echo "blurred and equalised"
-python -m src.q3 --transform blur_equal
+python -m src.q3 --transform blur_equal --lr 5e-4
 
 echo && echo "edge detected"
-python -m src.q3 --transform edge_detect
+python -m src.q3 --transform edge_detect --lr 5e-4
 
 echo && echo "hog features"
-python -m src.q3 --transform hog_feat
+python -m src.q3 --transform hog_feat --lr 5e-4
